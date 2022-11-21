c04332371267f5ddc62f6103dd7191475c7595aa
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2022-11-20 23:26:22 -0400

    first commit

---
+++ src/main/java/bo/edu/ucb/sis213/manfred/ManfredApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis213.manfred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManfredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManfredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/sis213/manfred/ManfredApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis213.manfred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManfredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
