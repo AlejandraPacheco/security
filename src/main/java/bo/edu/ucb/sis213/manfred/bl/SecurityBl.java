10ddba2983e26d6d07cc6e019b0595c67b3038ca
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2022-11-26 13:15:42 -0400

    Se genera el token principal con JWT

--- src/main/java/bo/edu/ucb/sis213/manfred/bl/SecurityBl.java
+++ src/main/java/bo/edu/ucb/sis213/manfred/bl/SecurityBl.java
@@ -6,7 +6,12 @@ import bo.edu.ucb.sis213.manfred.dto.AuthReqDto;
 import bo.edu.ucb.sis213.manfred.dto.AuthResDto;
 import bo.edu.ucb.sis213.manfred.dto.PersonDto;
 import bo.edu.ucb.sis213.manfred.entity.Person;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class SecurityBl {
@@ -31,6 +36,7 @@ public class SecurityBl {
     // El siguiente metodo realiza la autenticacion del sistema, va a buscar al repositorio de BBDD
     // la contraseña del usuario y la compara con su equivelente en BCrypt
     public AuthResDto authenticate(AuthReqDto credentials){
+        System.out.println("Email: " + credentials.email());
         AuthResDto result = new AuthResDto();
         System.out.println("Comenzando el proceso de autenticacion con: " + credentials);
         String currentPasswordInBCrypt = personDao.findByEmailAndPassword(credentials.email());
@@ -44,8 +50,9 @@ public class SecurityBl {
             if(bcryptResult.verified){
                 // Se genera el token
                 System.out.println("Las contraseñas coinciden, se genera el token");
-                result.setToken("TEST TOKEN");
-                result.setRefresh("TEST REFRESH TOKEN");
+                result = generateTokenJwt(credentials.email(), 300, new String[]{"admin", "user"});
+                //result.setToken("TEST TOKEN");
+                //result.setRefresh("TEST REFRESH TOKEN");
             } else{
                 System.out.println("Las contraseñas no coinciden");
                 throw new RuntimeException("Forbiden the secret and password are wrong");
@@ -59,5 +66,25 @@ public class SecurityBl {
 
     public void createPerson(){
 
+    }
+
+    private AuthResDto generateTokenJwt(String subject, int expirationTimeInSeconds, String [] roles){
+        AuthResDto result = new AuthResDto();
+        // Generamos el token principal
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("TigreCampeon2022");
+            String token = JWT.create()
+                    .withIssuer("ucb")
+                    .withSubject(subject)
+                    .withArrayClaim("roles", roles)
+                    .withExpiresAt(new Date(System.currentTimeMillis() + (expirationTimeInSeconds * 1000)))
+                    .sign(algorithm);
+            result.setToken(token);
+            result.setRefresh("TEST REFRESH TOKEN");
+        } catch (JWTCreationException exception){
+            //Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException("Error al generar el token", exception);
+        }
+        return result;
     }
 }
